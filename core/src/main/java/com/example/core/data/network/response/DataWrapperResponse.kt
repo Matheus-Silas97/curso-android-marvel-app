18da1d50bfb0f02a014e0194be23e0f8e62f3087
Matheus_Silas <matheus.silas123@gmail.com>
2022-10-19 16:49:21 -0300

    mapeamento dos objetoas de response da api da marvel

---
+++ core/src/main/java/com/example/core/data/network/response/DataWrapperResponse.kt
@@ -0,0 +1,8 @@
+package com.example.core.data.network.response
+
+data class DataWrapperResponse(
+
+    val copyright: String,
+    val data: DataContainerResponse
+
+)
---
+++ core/src/main/java/com/example/core/data/network/response/thumbnailResponse.kt
@@ -0,0 +1,9 @@
+package com.example.core.data.network.response
+
+data class thumbnailResponse(
+
+    val path: String,
+
+    val extension: String
+
+)
